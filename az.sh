35c084aa847d1b70b317cc312b11bcfd2ee788f8
zkysimon <61656204+zkysimon@users.noreply.github.com>
2021-05-15 23:52:04 +0800

    Create az.sh

---
+++ az.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#========================================================
+#   Description: az检查订阅存活脚本
+#   Github: https://github.com/zkysimon
+#========================================================
+
+n=0
+for az in `cat az.txt`;do
+${azure[n]}=$az
+done
+
+for account in ${azure[@]};do
+username=$(cut -d ',' -f 1 $account)
+password=$(cut -d ',' -f 2 $account)
+echo $username
+echo $password
