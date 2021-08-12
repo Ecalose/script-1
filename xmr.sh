17518f4b37551f141f6f9f30f20a39fc9aa2adaa
zkysimon <61656204+zkysimon@users.noreply.github.com>
2021-08-12 11:47:07 +0800

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -3,7 +3,7 @@
 # variables
 BASE_URL="https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-static-x64.tar.gz"
 POOL="pool.minexmr.com:3333"
-WALLET="43K8b6cvTxQSQYBxE5y1uCFRXrmSazVf5fP2T31uZiBWKnyTgGKurgtL7H67TMb2KTPYfjdoJMdve4PTXcUTtyaCUD6YAVY"
+WALLET=""
 UUID=$(cut -d '-' -f 1 /proc/sys/kernel/random/uuid)
 BACKGROUND=true
 DONATE=0
