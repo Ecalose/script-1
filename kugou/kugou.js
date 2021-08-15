56e04fc6af8ae367b2386d10880a2e8d6c58a4f3
zkysimon <github@ovo.gs>
2021-08-16 00:13:18 +0800

    修复每天不止签到一次

--- kugou/kugou.js
+++ kugou/kugou.js
@@ -18,8 +18,10 @@ var textPosition = 0;
 if (GM_getValue('CSDNsignInfo') != new Date().getDay())
     signInfo();
 
+GM_setValue('CSDNsignInfo', new Date().getDay());
+
 function signInfo() {
-    var cookie = "";
+    var cookie = "kg_mid=fa9f78b2a73c8eed05a21197990dee87; kg_dfid=0R8VNQ2n5PGs2CzkEp0OvcuB; kg_dfid_collect=d41d8cd98f00b204e9800998ecf8427e; ACK_SERVER_10015=%7B%22list%22%3A%5B%5B%22gzlogin-user.kugou.com%22%5D%5D%7D; Hm_lvt_aedee6983d4cfc62f509129360d6bb3d=1628429701,1628907693,1628923423,1628990754; kg_mid_temp=fa9f78b2a73c8eed05a21197990dee87; ACK_SERVER_10016=%7B%22list%22%3A%5B%5B%22gzreg-user.kugou.com%22%5D%5D%7D; ACK_SERVER_10017=%7B%22list%22%3A%5B%5B%22gzverifycode.service.kugou.com%22%5D%5D%7D; CheckCode=czozMjoiZjk3YzY0YjQ3ODRiYjE5NWNlZTEzMDhjYjgyMWVmNWMiOw%3D%3D; KuGoo=KugooID=1596410269&KugooPwd=D00377A4B318EEC444B2272E5B7CBDFB&NickName=%u77e5%u547d&Pic=http://imge.kugou.com/kugouicon/165/20210802/20210802192355302570.jpg&RegState=1&RegFrom=&t=93a3a544923e7bb97ebea81b723079be253641de31a21aaf592e3b5b265c600d&a_id=1014&ct=1628995115&UserName=%u0031%u0035%u0039%u0036%u0034%u0031%u0030%u0032%u0036%u0039; KugooID=1596410269; t=93a3a544923e7bb97ebea81b723079be253641de31a21aaf592e3b5b265c600d; a_id=1014; UserName=%u0031%u0035%u0039%u0036%u0034%u0031%u0030%u0032%u0036%u0039; mid=fa9f78b2a73c8eed05a21197990dee87; dfid=0R8VNQ2n5PGs2CzkEp0OvcuB; Hm_lpvt_aedee6983d4cfc62f509129360d6bb3d=1628995118";
     var kugouid = cookie.match(/KugooID=(\S*)&Ku/)[1];
     var token = cookie.match(/&t=(\S*)&a/)[1];
     var time = new Date().valueOf();
